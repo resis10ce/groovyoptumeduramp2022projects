1a423bf76cfb2dd7e2c52a3eaadd0c85a40916cd
eswaribala <parameswaribala@gmail.com>
2022-03-01 17:37:12 +0530

    updated

--- day2project/src/com/optum/day2project/utilities/XMLReaderDemo.groovy
+++ day2project/src/com/optum/day2project/utilities/XMLReaderDemo.groovy
@@ -2,9 +2,14 @@ package com.optum.day2project.utilities
 @Grab('org.codehaus.groovy:groovy-xml:3.0.9')
 import groovy.xml.XmlSlurper
 filePath="src/com/optum/day2project/resources/product.xml"
-
+//Approach 1
 def products=new XmlSlurper().parse(new File(filePath))
 
 for(Object obj : products.Product) {
 	println "$obj.Name ===> $obj.Price"
+}
+//Approach2
+def productsData=new XmlParser().parse(new File(filePath))
+productsData.Product.each { 
+	println "Product Name= ${it.Name}"
 }
