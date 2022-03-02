c21b84582cdcfe040b5f901f1d42c515b7512f50
eswaribala <parameswaribala@gmail.com>
2022-03-02 14:58:18 +0530

    updated

---
+++ day3project/src/com/optum/day3project/parsers/jsonparserdemo.groovy
@@ -0,0 +1,17 @@
+package com.optum.day3project.parsers
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+//run as java application
+if(args.size()<=0)
+	println "Not Exists"
+
+fileName=args[0]
+
+println "$fileName"
+
+//json string to json object
+jsonObject=new JsonSlurper().parseText('{"customerId":27469,"name":"Rathna"}')
+
+println "$jsonObject.customerId,$jsonObject.name"
+
