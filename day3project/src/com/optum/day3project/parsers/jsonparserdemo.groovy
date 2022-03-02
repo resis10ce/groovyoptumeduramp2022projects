043942106fc703da84258d7c5f454450a2c20b5d
eswaribala <parameswaribala@gmail.com>
2022-03-02 15:15:49 +0530

    updated

--- day3project/src/com/optum/day3project/parsers/jsonparserdemo.groovy
+++ day3project/src/com/optum/day3project/parsers/jsonparserdemo.groovy
@@ -11,8 +11,26 @@ println "$fileName"
 
 //json string to json object
 jsonObject=new JsonSlurper().parseText('{"customerId":27469,"name":"Rathna"}')
-
 println "$jsonObject.customerId,$jsonObject.name"
+
+//json object to string
+def patientInfo=["patientId": 94395,"doa":"2022-03-03","mobileNo":9952032786]
+String patientJson=JsonOutput.toJson(patientInfo)
+println "Patient Information=${patientJson}"
+
+//check file exists
+File file = new File(fileName)
+def lines = file.readLines()
+//println "$lines"
+
+//reading the json data from file
+def users=new JsonSlurper().parse(new File(fileName))
+users.each { 
+	user->
+	println "$user.name,$user.email"
+}
+
+
 
 
 
